2c7082728b1619bb408d8ecd9b25add0ae3a3774
Luapix <luapixel@gmail.com>
2020-04-29 19:16:59 +0200

    Added proper CLI with Docopt; added option to strip debug symbols or output somewhere specific

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
-use std::env;
+extern crate docopt;
+
 use std::fmt::{Display, Debug};
 use std::fs::read_to_string;
-use std::path::Path;
+use std::path::{Path, PathBuf};
+
+use docopt::Docopt;
 
 use hissy_lib::parser;
 use hissy_lib::parser::{lexer::{Tokens, read_tokens}, ast::ProgramAST};
@@ -38,12 +41,12 @@ fn parse(file: &str) -> Result<ProgramAST, String> {
 	format_error(parser::parse(&contents), "Parse error")
 }
 
-fn compile(file: &str, debug_info: bool) -> Result<String, String> {
-	let code = format_error(read_to_string(file), "Unable to open file")?;
+fn compile(input: &str, output: Option<String>, debug_info: bool) -> Result<String, String> {
+	let code = format_error(read_to_string(input), "Unable to open file")?;
 	let compiler = Compiler::new(debug_info);
 	
 	let program = format_error(compiler.compile_program(&code), "Compile error")?;
-	let output = Path::new(file).with_extension("hsyc");
+	let output = output.map_or_else(|| Path::new(input).with_extension("hsyc"), |o| PathBuf::from(o));
 	let res = program.to_file(output.clone());
 	format_error(res.map(|()| format!("Compiled into {:?}", output)), "Compile error")
 }
@@ -77,18 +80,56 @@ fn run(file: &str) -> Result<(), String> {
 	Ok(())
 }
 
-fn main() {
-	let args: Vec<String> = env::args().collect();
-	if args.len() == 3 {
-		match args[1].as_str() {
-			"lex" => return display_result(lex(&args[2])),
-			"parse" => return debug_result(parse(&args[2])),
-			"compile" => return display_result(compile(&args[2], true)),
-			"list" => return list(&args[2]),
-			"interpret" => return display_error(interpret(&args[2])),
-			"run" => return display_error(run(&args[2])),
-			_ => println!("Unknown command {:?}", args[1])
-		}
+
+const USAGE: &str = "
+Usage:
+  hissy interpret <code>
+  hissy run <bytecode>
+  hissy compile [--strip] [-o <bytecode>] <code>
+  hissy (lex|parse) <code>
+  hissy list <bytecode>
+  hissy --help
+  hissy --version
+
+Arguments:
+  <code>       A Hissy source file (usually .hsy)
+  <bytecode>   A Hissy bytecode file (usually .hsyc)
+
+Options:
+  --strip      Strip debug symbols from output
+  -o           Specifies the path of the resulting bytecode
+  --help       Print this help message
+  --version    Print the version
+";
+
+fn get_arg_option(args: &docopt::ArgvMap, key: &str) -> Option<String> {
+	if args.get_bool(key) {
+		Some(args.get_str(key).to_string())
+	} else {
+		None
 	}
-	println!("Usage: hissy lex|parse|compile|list|interpret|run <file>");
+}
+
+fn main() {
+	let args = Docopt::new(USAGE)
+		.and_then(|d| d.parse())
+		.unwrap_or_else(|e| e.exit());
+	
+	if args.get_bool("lex") {
+		display_result(lex(args.get_str("<code>")));
+	} else if args.get_bool("parse") {
+		debug_result(parse(args.get_str("<code>")));
+	} else if args.get_bool("compile") {
+		display_result(compile(args.get_str("<code>"), get_arg_option(&args, "<bytecode>"), !args.get_bool("--strip")));
+	} else if args.get_bool("list") {
+		list(args.get_str("<bytecode>"));
+	} else if args.get_bool("interpret") {
+		display_error(interpret(args.get_str("<code>")));
+	} else if args.get_bool("run") {
+		display_error(run(args.get_str("<bytecode>")));
+	} else if args.get_bool("--version") {
+		println!("Hissy v{}", env!("CARGO_PKG_VERSION"));
+	} else {
+		panic!("Unimplemented command");
+	}
 }
